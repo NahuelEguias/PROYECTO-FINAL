20fbb107463ae1b06bac7635e9518581bf4dea37
Nahuel Eguias <eguiasnn@yahoo.com.ar>
2023-05-31 23:51:14 -0300

    actualización proyecto final

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,12 +1,26 @@
 var express = require('express');
 const session = require('express-session');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
-router.get('/', function (req, res, next) {
+
+//Página de novedades
+router.get('/', async function (req, res, next) {
+
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades', {
         layout: 'admin/layout',
-        usuario: req.session.nombre
+        usuario: req.session.nombre,
+        novedades
     });
 });
+
+//para eliminar una novedad*/
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades')
+}); //cierra GET de eliminar
 
 module.exports = router;
