9d34c6ce17b5b4bf74293ea59be20c25c61ddb31
Nahuel Eguias <eguiasnn@yahoo.com.ar>
2023-06-07 19:58:40 -0300

    modificación de proyecto

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var novedadesModel = require('../models/novedadesModel');
 /* GET home page. */
 router.get('/', async function (req, res, next) {
 
-  var novedades = await novedadesModel.getNovedades
+  var novedades = await novedadesModel.getNovedades();
 
   res.render('index', {
     novedades
